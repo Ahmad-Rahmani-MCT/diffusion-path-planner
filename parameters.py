b85182ef0ae2e0481f8dcda3fcac3ab577469a39
Ahmad Rahmani <s320209@studenti.polito.it>
2026-07-17 16:35:45 +0200

    updared the scripts

---
+++ parameters.py
@@ -0,0 +1,11 @@
+## PARAMETERS FOR DATASET CREATION ## 
+
+NUM_SAMPLES = 100 # number of samples in the generated dataset
+GRID_SIZE = 64 # size of the side of the square map/grid 
+NUM_OBSTACLES = 10 # number of obstacles 
+MIN_RADIUS = 3 # minimum radius of obstacles 
+MAX_RADIUS = 4 # maximum radius of obstacles 
+SAFETY_PIXEL = 2 # obstacle dilation iteration
+NUM_POINTS_PATH = 64 # number of points in the generated path 
+NP_DATA_MAP_NAME = "maps" # name of the map data when saving in .npz
+NP_DATA_PATH_NAME = "paths" # name of the path data when saving in .npz
--- sample_creation.py
+++ sample_creation.py
@@ -1,31 +1,25 @@
-from functions import *
-
-NUM_SAMPLES = 1000 
-GRID_SIZE = 64 
-NUM_OBSTACLES = 10 
-MIN_RADIUS = 3 
-MAX_RADIUS = 4 
-NUM_POINTS_PATH = 64 
-
+import numpy as np
+from functions import generate_single_sample 
+from parameters import *
 
 print(f"generating {NUM_SAMPLES} dataset samples")
 
 all_maps = []
-all_trajectories = []
+all_paths = []
 
 for i in range(NUM_SAMPLES):
     if i % 100 == 0:
         print(f"Generated {i} / {NUM_SAMPLES}")
         
-    m, t = generate_single_sample(grid_size=GRID_SIZE, num_obstacles=NUM_OBSTACLES, min_radius=MIN_RADIUS, max_radius=MAX_RADIUS, num_points_path=NUM_POINTS_PATH)
+    m, t = generate_single_sample(grid_size=GRID_SIZE, num_obstacles=NUM_OBSTACLES, min_radius=MIN_RADIUS, max_radius=MAX_RADIUS, safety_pixel=SAFETY_PIXEL, num_points_path=NUM_POINTS_PATH)
     all_maps.append(m)
-    all_trajectories.append(t)
+    all_paths.append(t)
     
 maps_np = np.array(all_maps, dtype=np.float32)
-traj_np = np.array(all_trajectories, dtype=np.float32)
+paths_np = np.array(all_paths, dtype=np.float32)
 
-np.savez_compressed('map_path_dataset.npz', maps=maps_np, trajectories=traj_np)
+np.savez_compressed('map_path_dataset.npz', **{NP_DATA_MAP_NAME: maps_np, NP_DATA_PATH_NAME: paths_np})
 
 print("dataset saved to 'map_path_dataset.npz'")
 print(f"maps shape: {maps_np.shape}")
-print(f"trajectories shape: {traj_np.shape}")
+print(f"trajectories shape: {paths_np.shape}")

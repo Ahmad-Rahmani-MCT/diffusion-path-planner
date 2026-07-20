b454f1090d319ebf070f34f87d26416ebe029adc
Ahmad Rahmani <s320209@studenti.polito.it>
2026-07-20 17:53:00 +0200

    updating the models script and the parameters script

--- nn_models.py
+++ nn_models.py
@@ -1,67 +1,117 @@
 import torch
 import torch.nn as nn
-from diffusers import UNet1DModel 
+import torch.nn.functional as F
 
-class MapEncoder(nn.Module): 
+class MapEncoder(nn.Module):
 
-    def __init__(self, embedding_dim=128):
-        super().__init__() 
-
-        self.cnn = nn.Sequential(
-            # input [batch, 3, 64, 64] 
-            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1), # output [batch, 32, 32, 32]
-            nn.BatchNorm2d(32),
-            nn.ReLU(), 
-
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1), # output: [Batch, 64, 16, 16]
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), # output: [Batch, 128, 8, 8]
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-
-            nn.Flatten(), # flatten 3d to 1d
-
-            # 128*8*8
-            nn.Linear(128*8*8, embedding_dim), # output [batch, embedding_dim]
-            nn.ReLU()
-            )
-    
-    def forward(self, x): 
-        return self.cnn(x)
-    
-class DiffusionPlanner(nn.Module):
-
-    def __init__(self, condition_dim=128, sample_size=64, in_channels=2, out_channels=2):
+    def __init__(self, map_embedding_dim):
         super().__init__()
 
-        self.map_encoder = MapEncoder(embedding_dim=condition_dim) 
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(128*8*8, map_embedding_dim),
+            nn.Tanh()
+        )
 
-        self.unet = UNet1DModel(
-            sample_size= sample_size, # path length 
-            in_channels= in_channels + condition_dim, # x and y
-            out_channels= out_channels, # x and y
-            down_block_types=("DownBlock1D", "AttnDownBlock1D", "DownBlock1D"),
-            up_block_types=("UpBlock1D", "AttnUpBlock1D", "UpBlock1D"), 
-            block_out_channels=(64, 128, 256),
-        )   
+    def forward(self, x):
+        return self.cnn(x)
 
-    def forward(self, noisy_path, timestep, map_image): 
+class Conv1DBlock(nn.Module):
+
+    """
+    1D convolutional block with conditioning.
+    Conv1d -> relu -> add time + map condition -> Conv1d -> relu
+    """
+    
+    def __init__(self, in_channels, out_channels, time_embedding_dim, map_embedding_dim):
+        super().__init__()
+
+        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+
+        # projectors for time and map embeddings
+        self.time_mlp = nn.Linear(time_embedding_dim, out_channels)
+        self.map_mlp  = nn.Linear(map_embedding_dim, out_channels)
+
+    def forward(self, x, time_embedding_dim, map_embedding_dim):
         
-        # noisy_trajectory: shape [Batch, 2, 50]
-        # timestep: shape [Batch]
-        # map_image: shape [Batch, 3, 64, 64] 
+        h = self.relu(self.conv1(x))
 
-        map_features = self.map_encoder(map_image) # summarize map to [batch, 128]  
+        # inject time and map conditions
+        t_cond   = self.time_mlp(time_embedding_dim).unsqueeze(-1) # (B, out_channels, 1)
+        map_cond = self.map_mlp(map_embedding_dim).unsqueeze(-1) # (B, out_channels, 1)
+        h = h + t_cond + map_cond
 
-        map_features_expanded = map_features.unsqueeze(-1).repeat(1, 1, noisy_path.shape[-1]) # [batch, 128,50]
+        # second convolution
+        h = self.relu(self.conv2(h))
 
-        model_input = torch.cat([noisy_path, map_features_expanded], dim=1)
+        return h
 
-        noise_pred = self.unet(
-            sample=model_input,
-            timestep=timestep,
-        ).sample  
+class CustomConditionalUNet(nn.Module):
+    def __init__(self, map_embedding_dim, time_embedding_dim):
+        super().__init__()
 
+        # time embedding layer
+        self.time_embed = nn.Sequential(
+            nn.Linear(1, time_embedding_dim),
+            nn.ReLU(),
+            nn.Linear(time_embedding_dim, time_embedding_dim)
+        )
+
+        # down path (changing feature sizes)
+        self.down1 = Conv1DBlock(2, 64, self.time_dim, map_embedding_dim)
+        self.down2 = Conv1DBlock(64, 128, self.time_dim, map_embedding_dim)
+        self.down3 = Conv1DBlock(128, 256, self.time_dim, map_embedding_dim)
+
+        # upward path
+        self.up1 = Conv1DBlock(256 + 128, 128, self.time_dim, map_embedding_dim)
+        self.up2 = Conv1DBlock(128 + 64, 64, self.time_dim, map_embedding_dim)
+
+        # Final prediction layer
+        self.final_conv = nn.Conv1d(64, 2, kernel_size=1)
+
+    def forward(self, x, timestep, map_features):
+        # process time
+        t = timestep.float().unsqueeze(-1) # (B, 1)
+        t_emb = self.time_embed(t) # (B, 64)
+
+        # down
+        d1 = self.down1(x, t_emb, map_features)
+        d1_pool = F.avg_pool1d(d1, kernel_size=2)
+
+        d2 = self.down2(d1_pool, t_emb, map_features)
+        d2_pool = F.avg_pool1d(d2, kernel_size=2)
+
+        mid = self.down3(d2_pool, t_emb, map_features)
+
+        # up
+        u1 = F.interpolate(mid, scale_factor=2, mode='nearest')
+        u1 = torch.cat([u1, d2], dim=1) # skip connection from encoder
+        u1 = self.up1(u1, t_emb, map_features)
+
+        u2 = F.interpolate(u1, scale_factor=2, mode='nearest')
+        u2 = torch.cat([u2, d1], dim=1) # skip connection from encoder
+        u2 = self.up2(u2, t_emb, map_features)
+
+        # output
+        return self.final_conv(u2)
+
+class DiffusionPlanner(nn.Module):
+    def __init__(self, map_embedding_dim, time_embedding_dim):
+        super().__init__()
+
+        self.map_encoder = MapEncoder(map_embedding_dim=map_embedding_dim)
+        self.unet = CustomConditionalUNet(map_embedding_dim=map_embedding_dim, time_embedding_dim=time_embedding_dim)
+
+    def forward(self, noisy_path, timestep, map_image):
+
+        map_features = self.map_encoder(map_image)
+        noise_pred = self.unet(noisy_path, timestep, map_features)
         return noise_pred
--- parameters.py
+++ parameters.py
@@ -1,5 +1,4 @@
 ## PARAMETERS FOR DATASET CREATION ## 
-
 NUM_SAMPLES = 100 # number of samples in the generated dataset
 GRID_SIZE = 64 # size of the side of the square map/grid 
 NUM_OBSTACLES = 10 # number of obstacles 
@@ -10,3 +9,7 @@ NUM_POINTS_PATH = 64 # number of points in the generated path
 NP_DATA_MAP_NAME = "maps" # name of the map data when saving in .npz
 NP_DATA_PATH_NAME = "paths" # name of the path data when saving in .npz
 MAP_PATH_DATSET_NAME = "map_path_dataset.npz" # name of saving the dataset
+
+## PARAMETERS FOR THE DIFFUSION MODEL ## 
+MAP_EMBEDDING_DIM = 16 # map encoding dimension 
+TIME_EMBEDDING_DIM = 64 # time encoding dimension 
